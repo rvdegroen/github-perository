4d79ec83c703e307335164c0b66f1cd273c64b31
rvdegroen <rvdegroen@gmail.com>
2023-03-16 18:50:23 +0100

    removed getUsername function into dom module

--- scripts/app.js
+++ scripts/app.js
@@ -1,24 +1,13 @@
 import { renderPage } from "./modules/navigation.js";
-import { getUserRepositories } from "./modules/api";
-// import { getUserRepositories } from "./modules/api.js";
+import { getUserRepositories } from "./modules/api.js";
+
+// variables
+const $searchButton = document.getElementById("search__button");
 
 //render the new page on navigation
 window.onhashchange = () => renderPage();
 
-renderPage();
-
-// functions (put later in modules)
-
-// API LINK: https://api.github.com
-// "https://api.github.com/users/USER/repos"
-
-// modules: api.js: fetch API data
-
-export function getUsername() {
-  const $userInput = document.getElementById("user__input");
-  const username = $userInput.value;
-  return username;
-}
-
-const $searchButton = document.getElementById("search__button");
+// eventlisteners
 $searchButton.addEventListener("click", getUserRepositories);
+// calling functions
+renderPage();
