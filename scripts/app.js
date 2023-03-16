1f426ecfb57b4ddd4675cf5112c66d999d6968b8
rvdegroen <rvdegroen@gmail.com>
2023-03-16 21:55:16 +0100

    remove button and eventlistener and put into navigation.js

--- scripts/app.js
+++ scripts/app.js
@@ -1,13 +1,2 @@
-import { renderPage } from "./modules/navigation.js";
+import "./modules/navigation.js";
 import { getUserRepositories } from "./modules/api.js";
-
-// variables
-const $searchButton = document.getElementById("search__button");
-
-//render the new page on navigation
-window.onhashchange = () => renderPage();
-
-// eventlisteners
-$searchButton.addEventListener("click", getUserRepositories);
-// calling functions
-renderPage();
