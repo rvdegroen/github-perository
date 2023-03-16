9d8323f715639760d1ce434d463d6e419e0ac820
rvdegroen <rvdegroen@gmail.com>
2023-03-17 00:07:57 +0100

    moved error into navigation.js

--- scripts/modules/api.js
+++ scripts/modules/api.js
@@ -1,48 +1,34 @@
 // https://api.github.com/users/rvdegroen/repos
 export async function getUserRepositories(username) {
-	try {
-		// const username = getUsername();
-		// https://api.github.com/users/${username}/repos`
-		const response = await fetch(`https://api.github.com/users/${username}/repos`);
-		const json = await response.json();
-		// client side error
-		if (response.status >= 400 && response.status <= 499) {
-			const clientErrorMessage = json.message;
-			// new instance of error class
-			throw new Error(clientErrorMessage);
-		}
-		// return repos
-		console.log(json);
-		return json;
-	} catch (error) {
-		// server side error
-		console.log(error.message);
-		const $errorMessage = document.getElementById("error__message");
-		const errorMessage = error.message;
-		$errorMessage.textContent = errorMessage;
+	// try is in the navigation.js
+	// const username = getUsername();
+	// https://api.github.com/users/${username}/repos`
+	const response = await fetch(`https://api.github.com/users/${username}/repos`);
+	const json = await response.json();
+	// client side error
+	if (response.status >= 400 && response.status <= 499) {
+		const clientErrorMessage = json.message;
+		// new instance of error class
+		throw new Error(clientErrorMessage);
 	}
+	// return repos
+	console.log(json);
+	return json;
 }
 
 export async function getUser(username) {
-	try {
-		// const username = getUsername();
-		// https://api.github.com/users/${username}/repos`
-		const response = await fetch(`https://api.github.com/users/${username}`);
-		const json = await response.json();
-		// client side error
-		if (response.status >= 400 && response.status <= 499) {
-			const clientErrorMessage = json.message;
-			// new instance of error class
-			throw new Error(clientErrorMessage);
-		}
-		// return repos
-		console.log(json);
-		return json;
-	} catch (error) {
-		// server side error
-		console.log(error.message);
-		const $errorMessage = document.getElementById("error__message");
-		const errorMessage = error.message;
-		$errorMessage.textContent = errorMessage;
+	// try is in the navigation.js
+	// const username = getUsername();
+	// https://api.github.com/users/${username}/repos`
+	const response = await fetch(`https://api.github.com/users/${username}`);
+	const json = await response.json();
+	// client side error
+	if (response.status >= 400 && response.status <= 499) {
+		const clientErrorMessage = json.message;
+		// new instance of error class
+		throw new Error(clientErrorMessage);
 	}
+	// return repos
+	console.log(json);
+	return json;
 }
