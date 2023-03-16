d86a13052ba8f4d3f992ea5a4127529310e1afb9
rvdegroen <rvdegroen@gmail.com>
2023-03-16 18:44:32 +0100

    added getUserRepos into api module

--- scripts/modules/api.js
+++ scripts/modules/api.js
@@ -1,1 +1,24 @@
 // https://api.github.com/users/rvdegroen/repos
+export async function getUserRepositories() {
+  try {
+    const username = getUsername();
+    // https://api.github.com/users/${username}/repos`
+    const response = await fetch(`https://api.github.com/users/${username}/repos`);
+    const json = await response.json();
+    if (response.status >= 400 && response.status <= 499) {
+      // client side error
+      const clientErrorMessage = json.message;
+      // new instance of error class
+      throw new Error(clientErrorMessage);
+    }
+    // return repos
+    console.log(json);
+    return json;
+  } catch (error) {
+    // server side error
+    console.log(error.message);
+    const $errorMessage = document.getElementById("error__message");
+    const errorMessage = error.message;
+    $errorMessage.textContent = errorMessage;
+  }
+}
