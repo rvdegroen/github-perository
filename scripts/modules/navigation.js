4bc0150f5634cba3656c0049a87f87b17af92a4b
rvdegroen <rvdegroen@gmail.com>
2023-03-16 22:54:50 +0100

    added search button to details (not working)

--- scripts/modules/navigation.js
+++ scripts/modules/navigation.js
@@ -3,32 +3,43 @@ import { getUsername } from "./dom.js";
 import { getUserRepositories } from "./api.js";
 
 routie({
-  "": function () {
-    // code that runs when I got to index page
-    hideAllPages();
-    const $homepage = document.getElementById("homepage");
-    $homepage.classList.remove("hidden");
-    // variables
-    const $searchButton = document.getElementById("search__button");
-    // eventlisteners
-    $searchButton.addEventListener("click", () => {
-      const username = getUsername();
-      routie(`details/${username}`);
-    });
-  },
-  "details/:name": function (name) {
-    // code that runs when I got to index page
-    hideAllPages();
-    const $detailsPage = document.getElementById("details__page");
-    $detailsPage.classList.remove("hidden");
-    const allRepositories = getUserRepositories(name);
-    console.log(allRepositories);
-  },
+	"": function () {
+		// code that runs when I got to index page
+		hideAllPages();
+		const $homepage = document.getElementById("homepage");
+		// show page
+		$homepage.classList.remove("hidden");
+		// variables of search button
+		const $searchButton = document.getElementById("search__button");
+		// eventlisteners of search button
+		$searchButton.addEventListener("click", () => {
+			const username = getUsername();
+			routie(`details/${username}`);
+		});
+	},
+	"details/:name": function (name) {
+		// code that runs when I got to index page
+		hideAllPages();
+		const $detailsPage = document.getElementById("details__page");
+		// show page
+		$detailsPage.classList.remove("hidden");
+		// get all repositories of user
+		const allRepositories = getUserRepositories(name);
+		console.log(allRepositories);
+		// DOES NOT WORK PLS FIX IT
+		// variables of search button
+		const $searchButton = document.getElementById("search__button");
+		// eventlisteners of search button
+		$searchButton.addEventListener("click", () => {
+			const username = getUsername();
+			routie(`details/${username}`);
+		});
+	},
 });
 
 function hideAllPages() {
-  const $pages = document.querySelectorAll(".page");
-  for (const page of $pages) {
-    page.classList.add("hidden");
-  }
+	const $pages = document.querySelectorAll(".page");
+	for (const page of $pages) {
+		page.classList.add("hidden");
+	}
 }
