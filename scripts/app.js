f32d2e32f8d467686ed13b7c568fa8714250dd1c
rvdegroen <rvdegroen@gmail.com>
2023-03-16 17:24:31 +0100

    fetched in app.js temporarily

--- scripts/app.js
+++ scripts/app.js
@@ -1,15 +1,41 @@
 import { renderPage } from "./modules/navigation.js";
-import { getUserRepositories } from "./modules/api.js";
+// import { getUserRepositories } from "./modules/api.js";
 
 //render the new page on navigation
 window.onhashchange = () => renderPage();
 
 renderPage();
-getUserRepositories()
-  .then((data) => console.log(data))
-  .catch((reason) => console.log(reason.message));
 
-const $input = document.querySelector("input"),
-  $feedback = document.querySelector(".feedback");
+// functions (put later in modules)
 
-// https://api.github.com
+// API LINK: https://api.github.com
+// "https://api.github.com/users/USER/repos"
+
+// modules: api.js: fetch API data
+export async function getUserRepositories() {
+  try {
+    const username = getUsername();
+    // https://api.github.com/users/${username}/repos`
+    const response = await fetch(`https://api.github.com/users/${username}/repos`);
+    const json = await response.json();
+    //  return repos
+    console.log(json);
+    return json;
+  } catch (error) {
+    console.log(error.message);
+    const $errorMessage = document.getElementById("error__message");
+    const errorM = error.message;
+    $errorMessage.textContent = errorM;
+  }
+}
+
+getUserRepositories();
+
+export function getUsername() {
+  const $userInput = document.getElementById("#user__input");
+  const username = $userInput.value;
+  return username;
+}
+
+// const $searchButton = document.getElementById("#search__button");
+// $searchButton.addEventListener("click", getUserRepositories());
